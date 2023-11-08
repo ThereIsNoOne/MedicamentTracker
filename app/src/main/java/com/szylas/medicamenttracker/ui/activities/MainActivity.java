bbadeee95bf6a796b3989be15ff0c3290d3853f3
ThereIsNoOne <szymon.s.lasota@gmail.com>
2023-11-08 21:14:20 +0100

    Moved activities to specified package

--- app/src/main/java/com/szylas/medicamenttracker/ui/activities/MainActivity.java
+++ app/src/main/java/com/szylas/medicamenttracker/ui/activities/MainActivity.java
@@ -1,24 +1,30 @@
-package com.szylas.medicamenttracker;
+package com.szylas.medicamenttracker.ui.activities;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.ui.AppBarConfiguration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.szylas.medicamenttracker.R;
 import com.szylas.medicamenttracker.databinding.ActivityMainBinding;
 import com.szylas.medicamenttracker.datastore.TreatmentsReader;
-import com.szylas.medicamenttracker.models.Medicament;
+import com.szylas.medicamenttracker.models.meds.Pill;
 import com.szylas.medicamenttracker.models.Treatment;
+import com.szylas.medicamenttracker.ui.adapters.MedListAdapter;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
+
     private ActivityMainBinding binding;
+
+    private ArrayList<Treatment> treatmentList;
+    private int currentTreatment = 0;
+    private MedListAdapter medListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +34,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+
+        treatmentList = TreatmentsReader.loadTreatments(getAssets());
+        medListAdapter = new MedListAdapter(treatmentList.get(currentTreatment));
+
+        setupMedRecyclerView();
+    }
+
+    private void setupMedRecyclerView() {
+        RecyclerView medRecyclerView = findViewById(R.id.med_recycler_view);
+
+        medRecyclerView.setAdapter(medListAdapter);
+        medRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        System.out.println("Menu");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -47,9 +66,12 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.add_meds) {
+            treatmentList.get(currentTreatment).addMedicament(new Pill("Test pill", 1, 1));
+            medListAdapter.notifyItemInserted(treatmentList.get(currentTreatment).size() - 1);
+            return true;
         }
-        System.out.printf("User clicked %s", item);
+
         return super.onOptionsItemSelected(item);
     }
-
 }
